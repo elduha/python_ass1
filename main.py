f6a2fe0aa403ff7328834ff9bfb2b952fc9b6c94
elduha <79367834+elduha@users.noreply.github.com>
2021-09-23 12:29:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
-rep = cg.get_coins_markets(vs_currency='usd')
-print(report)
+crp = cg.get_coins_markets(vs_currency='usd')
+print(crp)
 
 asd = int(input("Enter: "))
 counter = 0
 while counter < asd:
     counter += 1
-    print(counter," - ", rep[counter]['name'], rep[counter]['market_cap'])
+    print(counter," - ", crp[counter]['name'],crp[counter]['market_cap'])
